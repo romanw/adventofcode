4f5d83cc48cd67e9b6cee3bf8aa09de2fe08c4e2
romanw <roman.woloszczuk@gmail.com>
2022-12-03 09:26:36 +0000

    added day 2 solution

--- 2022/02/main.py
+++ 2022/02/main.py
@@ -2,26 +2,49 @@
 
 file = open("input")
 
-def part1():
-    mostCals = 0
-    elf = 1
-    sum = 0
-    highCals = []
+def result1(a, b):
+    hs1 = ord(a) - ord("A") + 1
+    hs2 = ord(b) - ord("X") + 1
+    outcome = 0
+    if hs2 == hs1:
+        outcome = 3
+    elif (hs2 == 1 and hs1 == 3) or (hs2 == 2 and hs1 == 1) or (hs2 == 3 and hs1 == 2):
+        outcome = 6
+    return hs2 + outcome
+
+def toWin(a):
+    hs = a + 1
+    if hs > 3:
+        hs -= 3
+    return hs
+
+def toLose(a):
+    hs = a - 1
+    if hs < 1:
+        hs += 3
+    return hs
+    
+def result2(a, b):
+    res = 0
+    hs1 = ord(a) - ord("A") + 1
+    outcome = ord(b) - ord("X") + 1
+    if outcome == 1:
+        res = toLose(hs1)
+    if outcome == 2:
+        res = hs1 + 3
+    if outcome == 3:
+        res = toWin(hs1) + 6
+    return res
+    
+
+def scores():
+    totalScore1 = 0
+    totalScore2 = 0
     for line in file:
-        if line == "\n":
-            if sum > mostCals:
-                print(elf, sum)
-                mostCals = sum
-                highCals.append(sum)
-            elf += 1
-            sum = 0
-        else:
-            sum += int(line)
-    return highCals
-
-def part2(hc):
-    print(sum(hc[-3:]))
+        hs = line.strip().split()
+        totalScore1 += result1(hs[0], hs[1])
+        totalScore2 += result2(hs[0], hs[1])
+    print(totalScore1, totalScore2)
 
 
-hc = part1()
-part2(hc)
+scores()
